7a4955a7507b3580bc2b5166548b784bad40477f
Alejandro Navarro <drojannp@gmail.com>
2023-02-14 14:53:08 +0100

    Base de datos

---
+++ database/db.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS database_links;
+CREATE DATABASE database_links CHARSET utf8mb4;
+USE DATABASE database_links;
+
+CREATE TABLE users (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) NOT NULL, 
+    password VARCHAR(60) NOT NULL,
+    email VARCHAR (120) NOT NULL,
+    fullname VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE links (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    description TEXT,
+    user_id INT UNSIGNED,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREING KEY (user_id) REFERENCES users(id)
+);
