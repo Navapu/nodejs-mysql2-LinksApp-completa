3188c95c953111c5727033e08f8e98399e272581
Alejandro Navarro <drojannp@gmail.com>
2023-02-15 09:28:44 +0100

    Formulario añadido /links/add

--- routes/links.js
+++ routes/links.js
@@ -13,5 +13,7 @@ router.get('/', async (req, res, next) => {
 router.get('/add', (req, res) => {
   res.render('links/add')
 })
-
+router.post('/add', (req, res) => {
+  res.send('Recibido')
+})
 module.exports = router;
